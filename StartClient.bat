5465eba9888a2289af900a2744eb036e7db4163a
DimaKuzmin <scorpion26-10-96@yandex.ua>
2021-12-29 22:03:29 +0200

    Файлы fsGame и старт клиента

---
+++ StartClient.bat
@@ -0,0 +1,2 @@
+echo y|rmdir /s _client_\users\shaders_cache\
+start bins\xrEngine.exe -fsltx fsgameClient.ltx
---
+++ StartServer_omp.bat
@@ -0,0 +1,2 @@
+cd bins
+start ..\dedicated\xrEngine.exe -fsltx ..\fsgameDedicated.ltx -dedicated -i -start server(omp_test/rp/portsv=5400) client(localhost)
